94210f0400b26c75752dc1913a572e2e82db966d
Gabby <GabbyVByers@gmail.com>
2025-09-17 01:14:20 -0700

    the asserts make me feel warm

--- main.cpp
+++ main.cpp
@@ -69,9 +69,8 @@ SourceLine processRawSourceLine(std::string rawSourceLine)
 {
 	SourceLine sourceLine;
 	std::vector<std::string> tokens = tokenize(rawSourceLine);
-	std::string firstToken = tokens[0];
 
-	if (firstToken == "var")
+	if (tokens[0] == "var")
 	{
 		assert(tokens.size() == 2);
 		sourceLine.type = "VAR";
@@ -98,14 +97,17 @@ SourceLine processRawSourceLine(std::string rawSourceLine)
 		return;
 	}
 
-	if (firstToken == "label")
+	if (tokens[0] == "label")
 	{
 		assert(tokens.size() == 2);
 		sourceLine.labelAlias = tokens[1];
 		return;
 	}
 
-	if ((firstToken == "LDI") || (firstToken == "LDR") || (firstToken == "STR") || (firstToken == "ADI"))
+	if ((tokens[0] == "LDI") ||
+		(tokens[0] == "LDR") ||
+		(tokens[0] == "STR") ||
+		(tokens[0] == "ADI"))
 	{
 		assert((tokens.size() == 3) || (tokens.size() == 4));
 		assertValidRegister(tokens[1]);
@@ -122,6 +124,107 @@ SourceLine processRawSourceLine(std::string rawSourceLine)
 		if (tokens.size() == 4)
 		{
 			sourceLine.flag = tokens[3];
+		}
+
+		sourceLine.memorySize = 2;
+		return;
+	}
+
+	if ((tokens[0] == "LDX") ||
+		(tokens[0] == "STX") ||
+		(tokens[0] == "ADD") ||
+		(tokens[0] == "SUB") ||
+		(tokens[0] == "AND") ||
+		(tokens[0] == "ORR") ||
+		(tokens[0] == "XOR") ||
+		(tokens[0] == "CMP"))
+	{
+		assert((tokens.size() == 3) || (tokens.size() == 4));
+		assertValidRegister(tokens[1]);
+		assertValidRegister(tokens[2]);
+		if (tokens.size() == 4)
+		{
+			assertValidFlag(tokens[3]);
+		}
+
+		sourceLine.inst = tokens[0];
+		sourceLine.dst = tokens[1];
+		sourceLine.src = tokens[2];
+
+		if (tokens.size() == 4)
+		{
+			sourceLine.flag = tokens[3];
+		}
+
+		sourceLine.memorySize = 1;
+		return;
+	}
+
+	if ((tokens[0] == "SHR") ||
+		(tokens[0] == "SHL") ||
+		(tokens[0] == "KIN") ||
+		(tokens[0] == "GSP") ||
+		(tokens[0] == "PSH") ||
+		(tokens[0] == "POP"))
+	{
+		assert((tokens.size() == 2) || (tokens.size() == 3));
+		assertValidRegister(tokens[1]);
+
+		if (tokens.size() == 3)
+		{
+			assertValidFlag(tokens[2]);
+		}
+
+		sourceLine.inst = tokens[0];
+		sourceLine.dst = tokens[1];
+
+		if (tokens.size() == 3)
+		{
+			sourceLine.flag = tokens[2];
+		}
+
+		sourceLine.memorySize = 1;
+		return;
+	}
+
+	if ((tokens[0] == "DRW") ||
+		(tokens[0] == "RTS"))
+	{
+		assert((tokens.size() == 1) || (tokens.size() == 2));
+
+		if (tokens.size() == 2)
+		{
+			assertValidFlag(tokens[1]);
+		}
+
+		sourceLine.inst = tokens[0];
+
+		if (tokens.size() == 2)
+		{
+			sourceLine.flag = tokens[1];
+		}
+
+		sourceLine.memorySize = 1;
+		return;
+	}
+
+	if ((tokens[0] == "JMP") ||
+		(tokens[0] == "JSR"))
+	{
+		assert((tokens.size() == 2) || (tokens.size() == 3));
+		assertValidArgument(tokens[1]);
+
+		if (tokens.size() == 3)
+		{
+			assertValidFlag(tokens[2]);
+		}
+
+		sourceLine.inst = tokens[0];
+		sourceLine.argument = tokens[1];
+
+		if (tokens.size() == 3)
+		{
+			sourceLine.flag = tokens[2];
 		}
 
 		sourceLine.memorySize = 2;
