2cad13ee8cd4d0d69b21e0102e7a08eee97f6bdf
Gabby <GabbyVByers@gmail.com>
2025-10-05 14:07:41 -0700

    Added Support for HEX Output (Needed for FPGA)

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@
 #include <algorithm>
 #include <cctype>
 
-struct SourceLine
-{
+struct SourceLine {
 	unsigned int memoryAddress = 0;
 	unsigned int memorySize = 0;
 
@@ -23,8 +22,7 @@ struct SourceLine
 	std::string labelName    = "null";
 };
 
-void assertCharacterIsNumber(char ch)
-{
+void assertCharacterIsNumber(char ch) {
 	assert((ch == '0') ||
 		   (ch == '1') ||
 		   (ch == '2') ||
@@ -37,30 +35,22 @@ void assertCharacterIsNumber(char ch)
 		   (ch == '9'));
 }
 
-void assertAssemblyAndMemoryCoherence(std::vector<SourceLine>& sourceLines)
-{
-	for (SourceLine& line : sourceLines)
-	{
-		if (line.type == "ASM")
-		{
-			if (line.argument == "null")
-			{
+void assertAssemblyAndMemoryCoherence(std::vector<SourceLine>& sourceLines) {
+	for (SourceLine& line : sourceLines) {
+		if (line.type == "ASM") {
+			if (line.argument == "null") {
 				assert(line.memorySize == 1);
 			}
-			if (line.argument != "null")
-			{
+			if (line.argument != "null") {
 				assert(line.memorySize == 2);
 			}
 		}
 	}
-	for (SourceLine& line : sourceLines)
-	{
-		if (line.type == "ASM")
-		{
+	for (SourceLine& line : sourceLines) {
+		if (line.type == "ASM") {
 			assert(line.inst != "null");
 		}
-		if (line.type != "ASM")
-		{
+		if (line.type != "ASM") {
 			assert(line.inst == "null");
 			assert(line.dst == "null");
 			assert(line.src == "null");
@@ -70,51 +60,41 @@ void assertAssemblyAndMemoryCoherence(std::vector<SourceLine>& sourceLines)
 	}
 }
 
-void assertVariableDeclarationsAreEndOfSourceFile(std::vector<SourceLine>& sourceLines)
-{
+void assertVariableDeclarationsAreEndOfSourceFile(std::vector<SourceLine>& sourceLines) {
 	int numSourceLines = sourceLines.size();
 
 	bool haveSeenAVariable = false;
-	for (int i = 0; i < numSourceLines; i++)
-	{
+	for (int i = 0; i < numSourceLines; i++) {
 		SourceLine& sourceLine = sourceLines[i];
-		if (sourceLine.type == "VAR")
-		{
+		if (sourceLine.type == "VAR") {
 			haveSeenAVariable = true;
 		}
 
-		if (haveSeenAVariable)
-		{
-			if (sourceLine.type != "VAR")
-			{
+		if (haveSeenAVariable) {
+			if (sourceLine.type != "VAR") {
 				assert(false);
 			}
 		}
 	}
 }
 
-void assertNoDuplicateVariableDeclarations(std::vector<SourceLine>& sourceLines)
-{
+void assertNoDuplicateVariableDeclarations(std::vector<SourceLine>& sourceLines) {
 	int numSourceLines = sourceLines.size();
 
-	for (int i = 0; i < numSourceLines; i++)
-	{
+	for (int i = 0; i < numSourceLines; i++) {
 		SourceLine& sourceLine = sourceLines[i];
 		if (sourceLine.type != "VAR") continue;
 		std::string name = sourceLine.variableName;
 		assert(name != "null");
 
-		for (int j = 0; j < numSourceLines; j++)
-		{
+		for (int j = 0; j < numSourceLines; j++) {
 			SourceLine& otherSourceLine = sourceLines[j];
 			if (otherSourceLine.type != "VAR") continue;
 			std::string otherName = otherSourceLine.variableName;
 			assert(otherName != "null");
 
-			if (name == otherName)
-			{
-				if (i != j)
-				{
+			if (name == otherName) {
+				if (i != j) {
 					assert(false);
 				}
 			}
@@ -122,58 +102,48 @@ void assertNoDuplicateVariableDeclarations(std::vector<SourceLine>& sourceLines)
 	}
 }
 
-void assertNoDuplicateLabels(std::vector<SourceLine>& sourceLines)
-{
+void assertNoDuplicateLabels(std::vector<SourceLine>& sourceLines) {
 	int numSourceLines = sourceLines.size();
 
-	for (int i = 0; i < numSourceLines; i++)
-	{
+	for (int i = 0; i < numSourceLines; i++) {
 		SourceLine& sourceLine = sourceLines[i];
 		if (sourceLine.type != "LBL") continue;
 		std::string name = sourceLine.labelName;
 		assert(name != "null");
 
-		for (int j = 0; j < numSourceLines; j++)
-		{
+		for (int j = 0; j < numSourceLines; j++) {
 			SourceLine& otherSourceLine = sourceLines[j];
 			if (otherSourceLine.type != "LBL") continue;
 			std::string otherName = otherSourceLine.labelName;
 			assert(otherName != "null");
 
-			if (name == otherName)
-			{
+			if (name == otherName) {
 				if (i != j)
-				{
 					assert(false);
-				}
 			}
 		}
 	}
 }
 
-void assertTokenIsValidRegister(std::string token)
-{
+void assertTokenIsValidRegister(std::string token) {
 	assert((token == "r0") ||
 		   (token == "r1") ||
 		   (token == "r2") ||
 		   (token == "r3"));
 }
 
-void assertTokenIsValidArgument(std::string token)
-{
+void assertTokenIsValidArgument(std::string token) {
 	assert(token.size() >= 2);
 	if ((token[0] == '#') ||
 		(token[0] == '&') ||
-		(token[0] == '.'))
-	{
+		(token[0] == '.')) {
 		return;
 	}
 	std::cout << "\n CATASTROPHIC ERROR: Invalid Argument: " << token << "\n";
 	assert(false);
 }
 
-void assertTokenIsValidFlag(std::string token)
-{
+void assertTokenIsValidFlag(std::string token) {
 	assert((token == "FLAG_ZERO")          ||
 		   (token == "FLAG_EQUAL")         ||
 		   (token == "FLAG_GREATER_THAN")  ||
@@ -184,29 +154,24 @@ void assertTokenIsValidFlag(std::string token)
 		   (token == "!FLAG_LESS_THAN"));
 }
 
-std::vector<std::string> tokenize(const std::string& input)
-{
+std::vector<std::string> tokenize(const std::string& input) {
 	std::vector<std::string> result;
 	std::istringstream iss(input);
 	std::string word;
-	while (iss >> word)
-	{
+	while (iss >> word) {
 		result.push_back(word);
 	}
 	return result;
 }
 
-SourceLine processTokens(std::vector<std::string> tokens)
-{
+SourceLine processTokens(std::vector<std::string> tokens) {
 	SourceLine sourceLine;
 	assert(tokens.size() != 0);
 
-	if (tokens[0] == "var")
-	{
+	if (tokens[0] == "var") {
 		assert(tokens.size() == 2);
 		assert(tokens[1].size() != 0);
-		if (tokens[1].find('[') != std::string::npos)
-		{
+		if (tokens[1].find('[') != std::string::npos) {
 			size_t indexOpenBraket = tokens[1].find('[');
 			size_t indexCloseBraket = tokens[1].find(']');
 			assert(indexOpenBraket < indexCloseBraket - 1);
@@ -224,8 +189,7 @@ SourceLine processTokens(std::vector<std::string> tokens)
 			sourceLine.variableName = trueVariableName;
 			sourceLine.memorySize = std::stoul(memorySizeString);
 		}
-		else
-		{
+		else {
 			assert(tokens[1].find('[') == std::string::npos);
 			assert(tokens[1].find(']') == std::string::npos);
 			sourceLine.variableName = tokens[1];
@@ -235,8 +199,7 @@ SourceLine processTokens(std::vector<std::string> tokens)
 		return sourceLine;
 	}
 
-	if (tokens[0] == "label")
-	{
+	if (tokens[0] == "label") {
 		assert(tokens.size() == 2);
 		assert(tokens[1].size() != 0);
 		sourceLine.type = "LBL";
@@ -247,8 +210,7 @@ SourceLine processTokens(std::vector<std::string> tokens)
 	if ((tokens[0] == "LDI") ||
 		(tokens[0] == "LDR") ||
 		(tokens[0] == "STR") ||
-		(tokens[0] == "ADI"))
-	{
+		(tokens[0] == "ADI")) {
 		assert((tokens.size() == 3) || (tokens.size() == 4));
 		assertTokenIsValidRegister(tokens[1]);
 		assertTokenIsValidArgument(tokens[2]);
@@ -276,8 +238,7 @@ SourceLine processTokens(std::vector<std::string> tokens)
 		(tokens[0] == "AND") ||
 		(tokens[0] == "ORR") ||
 		(tokens[0] == "XOR") ||
-		(tokens[0] == "CMP"))
-	{
+		(tokens[0] == "CMP")) {
 		assert((tokens.size() == 3) || (tokens.size() == 4));
 		assertTokenIsValidRegister(tokens[1]);
 		assertTokenIsValidRegister(tokens[2]);
@@ -302,8 +263,7 @@ SourceLine processTokens(std::vector<std::string> tokens)
 		(tokens[0] == "KIN") ||
 		(tokens[0] == "GSP") ||
 		(tokens[0] == "PSH") ||
-		(tokens[0] == "POP"))
-	{
+		(tokens[0] == "POP")) {
 		assert((tokens.size() == 2) || (tokens.size() == 3));
 		assertTokenIsValidRegister(tokens[1]);
 		if (tokens.size() == 3) {
@@ -322,8 +282,7 @@ SourceLine processTokens(std::vector<std::string> tokens)
 	}
 
 	if ((tokens[0] == "DRW") ||
-		(tokens[0] == "RTS"))
-	{
+		(tokens[0] == "RTS")) {
 		assert((tokens.size() == 1) || (tokens.size() == 2));
 		if (tokens.size() == 2) {
 			assertTokenIsValidFlag(tokens[1]);
@@ -340,8 +299,7 @@ SourceLine processTokens(std::vector<std::string> tokens)
 	}
 
 	if ((tokens[0] == "JMP") ||
-		(tokens[0] == "JSR"))
-	{
+		(tokens[0] == "JSR")) {
 		assert((tokens.size() == 2) || (tokens.size() == 3));
 		assertTokenIsValidArgument(tokens[1]);
 		if ((tokens.size() == 3)) {
@@ -362,20 +320,17 @@ SourceLine processTokens(std::vector<std::string> tokens)
 	assert(false);
 }
 
-int main()
-{
+int main() {
 	std::vector<std::string> lines;
 	std::ifstream file("assembly.txt");
 	std::string line;
-	while (std::getline(file, line))
-	{
+	while (std::getline(file, line)) {
 		lines.push_back(line);
 	}
 	file.close();
 
 	std::vector<SourceLine> sourceLines;
-	for (std::string& line : lines)
-	{
+	for (std::string& line : lines) {
 		std::vector<std::string> tokens = tokenize(line);
 		if (tokens.size() == 0) continue;
 		if (tokens[0].substr(0, 2) == "//") continue;
@@ -388,19 +343,16 @@ int main()
 	assertNoDuplicateLabels(sourceLines);
 
 	unsigned int currentAddress = 0;
-	for (SourceLine& line : sourceLines)
-	{
+	for (SourceLine& line : sourceLines) {
 		line.memoryAddress = currentAddress;
 		currentAddress += line.memorySize;
 	}
 
-	for (SourceLine& line : sourceLines)
-	{
+	for (SourceLine& line : sourceLines) {
 		if (line.argument == "null") continue;
 		assert(line.type == "ASM");
 
-		if (line.argument[0] == '#')
-		{
+		if (line.argument[0] == '#') {
 			std::string trueArgument = line.argument.substr(1, line.argument.size() - 1);
 			assert((line.inst == "LDI") || (line.inst == "ADI"));
 			assert(trueArgument.size() >= 1);
@@ -409,18 +361,15 @@ int main()
 			continue;
 		}
 
-		if (line.argument[0] == '&')
-		{
+		if (line.argument[0] == '&') {
 			std::string targetVariable = line.argument.substr(1, line.argument.size() - 1);
 			assert((line.inst == "LDI") || (line.inst == "LDR") || (line.inst == "STR"));
 			assert(targetVariable.size() >= 1);
 
-			for (SourceLine& otherLine : sourceLines)
-			{
+			for (SourceLine& otherLine : sourceLines) {
 				if (otherLine.type != "VAR") continue;
 				assert(otherLine.variableName != "null");
-				if (otherLine.variableName == targetVariable)
-				{
+				if (otherLine.variableName == targetVariable) {
 					line.argument = std::to_string(otherLine.memoryAddress);
 					goto doubleContinue;
 				}
@@ -430,18 +379,15 @@ int main()
 			assert(false);
 		}
 
-		if (line.argument[0] == '.')
-		{
+		if (line.argument[0] == '.') {
 			std::string targetLabel = line.argument.substr(1, line.argument.size() - 1);
 			assert((line.inst == "JMP") || (line.inst == "JSR"));
 			assert(targetLabel.size() >= 1);
 
-			for (SourceLine& otherLine : sourceLines)
-			{
+			for (SourceLine& otherLine : sourceLines) {
 				if (otherLine.type != "LBL") continue;
 				assert(otherLine.labelName != "null");
-				if (otherLine.labelName == targetLabel)
-				{
+				if (otherLine.labelName == targetLabel) {
 					line.argument = std::to_string(otherLine.memoryAddress);
 					goto doubleContinue;
 				}
@@ -456,38 +402,37 @@ int main()
 	}
 
 	std::ofstream out("compiled_binary.bin", std::ios::binary);
-	for (SourceLine& line : sourceLines)
-	{
+	std::ofstream textOut("memory_init.txt");
+	for (SourceLine& line : sourceLines) {
 		assert(line.type != "null");
 		assert(sizeof(unsigned short) == 2);
 
-		if (line.type == "ASM")
-		{
+		if (line.type == "ASM") {
 			unsigned short machineCode = 0b0000000000000000;
 
-			if (line.inst == "LDI") { machineCode += 0b0000000000000000; }
-			if (line.inst == "LDR") { machineCode += 0b0000000100000000; }
-			if (line.inst == "STR") { machineCode += 0b0000001000000000; }
-			if (line.inst == "LDX") { machineCode += 0b0000001100000000; }
-			if (line.inst == "STX") { machineCode += 0b0000010000000000; }
-			if (line.inst == "MOV") { machineCode += 0b0000010100000000; }
-			if (line.inst == "SHR") { machineCode += 0b0000011000000000; }
-			if (line.inst == "SHL") { machineCode += 0b0000011100000000; }
-			if (line.inst == "ADI") { machineCode += 0b0000100000000000; }
-			if (line.inst == "ADD") { machineCode += 0b0000100100000000; }
-			if (line.inst == "SUB") { machineCode += 0b0000101000000000; }
-			if (line.inst == "AND") { machineCode += 0b0000101100000000; }
-			if (line.inst == "ORR") { machineCode += 0b0000110000000000; }
-			if (line.inst == "XOR") { machineCode += 0b0000110100000000; }
-			if (line.inst == "CMP") { machineCode += 0b0000111000000000; }
-			if (line.inst == "KIN") { machineCode += 0b0000111100000000; }
-			if (line.inst == "DRW") { machineCode += 0b0001000000000000; }
-			if (line.inst == "JMP") { machineCode += 0b0001000100000000; }
-			if (line.inst == "JSR") { machineCode += 0b0001001000000000; }
-			if (line.inst == "RTS") { machineCode += 0b0001001100000000; }
-			if (line.inst == "GSP") { machineCode += 0b0001010000000000; }
-			if (line.inst == "POP") { machineCode += 0b0001010100000000; }
-			if (line.inst == "PSH") { machineCode += 0b0001011000000000; }
+			if (line.inst == "LDI") { machineCode += 0b0000000000000000; textOut << "00"; }
+			if (line.inst == "LDR") { machineCode += 0b0000000100000000; textOut << "01"; }
+			if (line.inst == "STR") { machineCode += 0b0000001000000000; textOut << "02"; }
+			if (line.inst == "LDX") { machineCode += 0b0000001100000000; textOut << "03"; }
+			if (line.inst == "STX") { machineCode += 0b0000010000000000; textOut << "04"; }
+			if (line.inst == "MOV") { machineCode += 0b0000010100000000; textOut << "05"; }
+			if (line.inst == "SHR") { machineCode += 0b0000011000000000; textOut << "06"; }
+			if (line.inst == "SHL") { machineCode += 0b0000011100000000; textOut << "07"; }
+			if (line.inst == "ADI") { machineCode += 0b0000100000000000; textOut << "08"; }
+			if (line.inst == "ADD") { machineCode += 0b0000100100000000; textOut << "09"; }
+			if (line.inst == "SUB") { machineCode += 0b0000101000000000; textOut << "0A"; }
+			if (line.inst == "AND") { machineCode += 0b0000101100000000; textOut << "0B"; }
+			if (line.inst == "ORR") { machineCode += 0b0000110000000000; textOut << "0C"; }
+			if (line.inst == "XOR") { machineCode += 0b0000110100000000; textOut << "0E"; }
+			if (line.inst == "CMP") { machineCode += 0b0000111000000000; textOut << "0E"; }
+			if (line.inst == "KIN") { machineCode += 0b0000111100000000; textOut << "0F"; }
+			if (line.inst == "DRW") { machineCode += 0b0001000000000000; textOut << "10"; }
+			if (line.inst == "JMP") { machineCode += 0b0001000100000000; textOut << "11"; }
+			if (line.inst == "JSR") { machineCode += 0b0001001000000000; textOut << "12"; }
+			if (line.inst == "RTS") { machineCode += 0b0001001100000000; textOut << "13"; }
+			if (line.inst == "GSP") { machineCode += 0b0001010000000000; textOut << "14"; }
+			if (line.inst == "POP") { machineCode += 0b0001010100000000; textOut << "15"; }
+			if (line.inst == "PSH") { machineCode += 0b0001011000000000; textOut << "16"; }
 
 			if (line.dst == "r0")   { machineCode += 0b0000000000000000; }
 			if (line.dst == "r1")   { machineCode += 0b0000000001000000; }
@@ -499,20 +444,46 @@ int main()
 			if (line.src == "r2")   { machineCode += 0b0000000000100000; }
 			if (line.src == "r3")   { machineCode += 0b0000000000110000; }
 
-			if (line.flag == "null")               { machineCode += 0b0000000000000000; }
-			if (line.flag == "FLAG_ZERO")          { machineCode += 0b0000000000001000; }
-			if (line.flag == "FLAG_EQUAL")         { machineCode += 0b0000000000001001; }
-			if (line.flag == "FLAG_GREATER_THAN")  { machineCode += 0b0000000000001010; }
-			if (line.flag == "FLAG_LESS_THAN")     { machineCode += 0b0000000000001011; }
-			if (line.flag == "!FLAG_ZERO")         { machineCode += 0b0000000000001100; }
-			if (line.flag == "!FLAG_EQUAL")        { machineCode += 0b0000000000001101; }
-			if (line.flag == "!FLAG_GREATER_THAN") { machineCode += 0b0000000000001110; }
-			if (line.flag == "!FLAG_LESS_THAN")    { machineCode += 0b0000000000001111; }
+			std::string DST = line.dst;
+			std::string SRC = line.src;
+			if ((DST == "r0") || (DST == "null")) {
+				if ((SRC == "r0") || (SRC == "null")) { textOut << "0"; }
+				if  (SRC == "r1")                     { textOut << "1"; }
+				if  (SRC == "r2")                     { textOut << "2"; }
+				if  (SRC == "r3")                     { textOut << "3"; } 
+			}
+			if  (DST == "r1") {
+				if ((SRC == "r0") || (SRC == "null")) { textOut << "4"; }
+				if  (SRC == "r1")                     { textOut << "5"; }
+				if  (SRC == "r2")                     { textOut << "6"; }
+				if  (SRC == "r3")                     { textOut << "7"; }
+			}
+			if  (DST == "r2") {
+				if ((SRC == "r0") || (SRC == "null")) { textOut << "8"; }
+				if  (SRC == "r1")                     { textOut << "9"; }
+				if  (SRC == "r2")                     { textOut << "A"; }
+				if  (SRC == "r3")                     { textOut << "B"; }
+			}
+			if  (DST == "r3") {
+				if ((SRC == "r0") || (SRC == "null")) { textOut << "C"; }
+				if  (SRC == "r1")                     { textOut << "D"; }
+				if  (SRC == "r2")                     { textOut << "E"; }
+				if  (SRC == "r3")                     { textOut << "F"; }
+			}
+
+			if (line.flag == "null")               { machineCode += 0b0000000000000000; textOut << "0\n"; }
+			if (line.flag == "FLAG_ZERO")          { machineCode += 0b0000000000001000; textOut << "8\n"; }
+			if (line.flag == "FLAG_EQUAL")         { machineCode += 0b0000000000001001; textOut << "9\n"; }
+			if (line.flag == "FLAG_GREATER_THAN")  { machineCode += 0b0000000000001010; textOut << "A\n"; }
+			if (line.flag == "FLAG_LESS_THAN")     { machineCode += 0b0000000000001011; textOut << "B\n"; }
+			if (line.flag == "!FLAG_ZERO")         { machineCode += 0b0000000000001100; textOut << "C\n"; }
+			if (line.flag == "!FLAG_EQUAL")        { machineCode += 0b0000000000001101; textOut << "D\n"; }
+			if (line.flag == "!FLAG_GREATER_THAN") { machineCode += 0b0000000000001110; textOut << "E\n"; }
+			if (line.flag == "!FLAG_LESS_THAN")    { machineCode += 0b0000000000001111; textOut << "F\n"; }
 
 			out.write((const char*)(&machineCode), sizeof(unsigned short));
 
-			if (line.argument != "null")
-			{
+			if (line.argument != "null") {
 				for (char ch : line.argument) { assertCharacterIsNumber(ch); }
 				assert(line.memorySize == 2);
 
@@ -523,19 +494,16 @@ int main()
 			}
 		}
 
-		if (line.type == "VAR")
-		{
+		if (line.type == "VAR") {
 			unsigned short zero = 0;
-			for (int i = 0; i < line.memorySize; i++)
-			{
+			for (int i = 0; i < line.memorySize; i++) {
 				out.write((const char*)(&zero), sizeof(unsigned short));
+				textOut << "0000\n";
 			}
 		}
 
 		if (line.type == "LBL")
-		{
 			continue;
-		}
 	}
 
 	return 0;
